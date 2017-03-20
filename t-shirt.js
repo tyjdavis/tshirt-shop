bc8b7ed9c4fadf1f9f9095515635af37f1864f48
Tyler Davis <tyjdavis@outlook.com>
2017-03-20 18:42:44 -0400

    js size selector added

---
+++ t-shirt.js
@@ -0,0 +1,48 @@
+function clickSizeXL(event) {
+  event.target.parentElement.parentElement.lastElementChild.textContent= "XL";
+}
+
+let sizeXL = document.querySelectorAll('.XL');
+  sizeXL[0].addEventListener('click', clickSizeXL);
+  sizeXL[1].addEventListener('click', clickSizeXL);
+  sizeXL[2].addEventListener('click', clickSizeXL);
+
+
+function clickSizeL(event) {
+  event.target.parentElement.parentElement.lastElementChild.textContent= "L";
+}
+
+  let sizeL = document.querySelectorAll('.L');
+    sizeL[0].addEventListener('click', clickSizeL);
+    sizeL[1].addEventListener('click', clickSizeL);
+    sizeL[2].addEventListener('click', clickSizeL);
+
+
+function clickSizeM(event) {
+  event.target.parentElement.parentElement.lastElementChild.textContent= "M";
+}
+  let sizeM = document.querySelectorAll('.M');
+    sizeM[0].addEventListener('click', clickSizeM);
+    sizeM[1].addEventListener('click', clickSizeM);
+    sizeM[2].addEventListener('click', clickSizeM);
+
+
+function clickSizeS(event) {
+  event.target.parentElement.parentElement.lastElementChild.textContent= "S";
+}
+  let sizeS = document.querySelectorAll('.S');
+    sizeS[0].addEventListener('click', clickSizeS);
+    sizeS[1].addEventListener('click', clickSizeS);
+    sizeS[2].addEventListener('click', clickSizeS);
+
+
+
+
+function clickColorBlue(event) {
+  event.target.parentElement.parentElement.lastElementChild.textContent= "XL";
+  }
+
+  let colorBlue = document.querySelectorAll('.blue-option');
+  colorBlue[0].addEventListener('click', clickColorBlue);
+  colorBlue[1].addEventListener('click', clickColorBlue);
+  colorBlue[2].addEventListener('click', clickColorBlue);
