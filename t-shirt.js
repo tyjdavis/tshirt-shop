48639e568a2bc4268f1440504bb0aee2f7c5bde8
Tyler Davis <tyjdavis@outlook.com>
2017-03-21 14:34:54 -0400

    colors working

--- t-shirt.js
+++ t-shirt.js
@@ -1,5 +1,6 @@
 //Size Selector
 
+
 function clickSizeXL(event) {
   event.target.parentElement.parentElement.lastElementChild.textContent= "XL";
 }
@@ -40,58 +41,117 @@ function clickSizeS(event) {
 
 //Color Selector
 
-/* function clickColorBlue(event) {
-  let myImage = document.querySelectorAll('img');
-  myImage[0].src = "shirts/blue-front.png";
-  }
 
-function clickColorBlue(event) {
-  let myImage = document.querySelectorAll('img');
-  myImage[1].src = "shirts/blue-front.png";
-  }
+//Blue Color
 
-function clickColorBlue(event) {
-  let myImage = document.querySelectorAll('img');
-  myImage[2].src = "shirts/blue-front.png";
-  } */
-
-function clickColorBlue(event) {
-  let myImage = document.querySelector('img');
+function clickColorBlue1() {
+  let myImage = document.querySelector('img.first');
   myImage.src = "shirts/blue-front.png";
   }
 
-  let colorBlue = document.querySelectorAll('.blue-option');
-  colorBlue[0].addEventListener('click', clickColorBlue);
-  colorBlue[1].addEventListener('click', clickColorBlue);
-  colorBlue[2].addEventListener('click', clickColorBlue);
+function clickColorBlue2() {
+  let myImage = document.querySelector('img.second');
+  myImage.src = "shirts/blue-front.png";
+  }
+
+function clickColorBlue3() {
+  let myImage = document.querySelector('img.third');
+  myImage.src = "shirts/blue-front.png";
+  }
+
+let colorBlue = document.querySelectorAll('.blue-option');
+colorBlue[0].addEventListener('click', clickColorBlue1);
+colorBlue[1].addEventListener('click', clickColorBlue2);
+colorBlue[2].addEventListener('click', clickColorBlue3);
 
 
-function clickColorPink(event) {
-  let myImage = document.querySelector('img');
+//Pink Color
+
+function clickColorPink1() {
+  let myImage = document.querySelector('img.first');
   myImage.src = "shirts/pink-front.png";
   }
 
-  let colorPink = document.querySelectorAll('.pink-option');
-  colorPink[0].addEventListener('click', clickColorPink);
-  colorPink[1].addEventListener('click', clickColorPink);
-  colorPink[2].addEventListener('click', clickColorPink);
+function clickColorPink2() {
+  let myImage = document.querySelector('img.second');
+  myImage.src = "shirts/pink-front.png";
+  }
 
-function clickColorOrange(event) {
-  let myImage = document.querySelector('img');
+function clickColorPink3() {
+  let myImage = document.querySelector('img.third');
+  myImage.src = "shirts/pink-front.png";
+  }
+
+let colorPink = document.querySelectorAll('.pink-option');
+colorPink[0].addEventListener('click', clickColorPink1);
+colorPink[1].addEventListener('click', clickColorPink2);
+colorPink[2].addEventListener('click', clickColorPink3);
+
+
+//Orange Color
+
+function clickColorOrange1() {
+  let myImage = document.querySelector('img.first');
   myImage.src = "shirts/yellow-front.png";
   }
 
-  let colorOrange = document.querySelectorAll('.orange-option');
-  colorOrange[0].addEventListener('click', clickColorOrange);
-  colorOrange[1].addEventListener('click', clickColorOrange);
-  colorOrange[2].addEventListener('click', clickColorOrange);
+function clickColorOrange2() {
+  let myImage = document.querySelector('img.second');
+  myImage.src = "shirts/yellow-front.png";
+  }
 
-function clickColorGreen(event) {
-  let myImage = document.querySelector('img');
+function clickColorOrange3() {
+  let myImage = document.querySelector('img.third');
+  myImage.src = "shirts/yellow-front.png";
+  }
+
+let colorOrange = document.querySelectorAll('.orange-option');
+colorOrange[0].addEventListener('click', clickColorOrange1);
+colorOrange[1].addEventListener('click', clickColorOrange2);
+colorOrange[2].addEventListener('click', clickColorOrange3);
+
+
+//Green Color
+
+function clickColorGreen1() {
+  let myImage = document.querySelector('img.first');
   myImage.src = "shirts/green-front.png";
   }
 
-  let colorGreen = document.querySelectorAll('.green-option');
-  colorGreen[0].addEventListener('click', clickColorGreen);
-  colorGreen[1].addEventListener('click', clickColorGreen);
-  colorGreen[2].addEventListener('click', clickColorGreen);
+function clickColorGreen2() {
+  let myImage = document.querySelector('img.second');
+  myImage.src = "shirts/green-front.png";
+  }
+
+function clickColorGreen3() {
+  let myImage = document.querySelector('img.third');
+  myImage.src = "shirts/green-front.png";
+  }
+
+let colorGreen = document.querySelectorAll('.green-option');
+colorGreen[0].addEventListener('click', clickColorGreen1);
+colorGreen[1].addEventListener('click', clickColorGreen2);
+colorGreen[2].addEventListener('click', clickColorGreen3);
+
+
+
+//Shopping Cart
+
+function buy1() {
+  let list = document.createElement ("li");
+  let shirt = document.createTextNode ("Save my Trees $29");
+  console.log("Save my Trees $29");
+}
+
+function buy2() {
+  console.log("Nature Lover $19");
+}
+
+function buy3() {
+  console.log("Forrest Walk $39");
+}
+
+let buyFirst = document.querySelectorAll(".buy");
+buyFirst[0].addEventListener('click', buy1);
+buyFirst[1].addEventListener('click', buy2);
+buyFirst[2].addEventListener('click', buy3);
