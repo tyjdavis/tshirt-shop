94e9f2e5ecbadaf6940ff3eabc6484d7cbfbcf0f
Tyler Davis <tyjdavis@outlook.com>
2017-03-20 20:23:54 -0400

    working on color change

--- t-shirt.js
+++ t-shirt.js
@@ -1,3 +1,5 @@
+//Size Selector
+
 function clickSizeXL(event) {
   event.target.parentElement.parentElement.lastElementChild.textContent= "XL";
 }
@@ -36,13 +38,40 @@ function clickSizeS(event) {
     sizeS[2].addEventListener('click', clickSizeS);
 
 
-
+//Color Selector
 
 function clickColorBlue(event) {
-  event.target.parentElement.parentElement.lastElementChild.textContent= "XL";
+  event.target.parentElement.parentElement.imageContent src= "shirts/blue-front.png";
   }
 
   let colorBlue = document.querySelectorAll('.blue-option');
   colorBlue[0].addEventListener('click', clickColorBlue);
   colorBlue[1].addEventListener('click', clickColorBlue);
   colorBlue[2].addEventListener('click', clickColorBlue);
+
+function clickColorPink(event) {
+  event.target.parentElement.parentElement.lastElementChild.imageContent= "XL";
+  }
+
+  let colorPink = document.querySelectorAll('.pink-option');
+  colorPink[0].addEventListener('click', clickColorPink);
+  colorPink[1].addEventListener('click', clickColorPink);
+  colorPink[2].addEventListener('click', clickColorPink);
+
+function clickColorOrange(event) {
+  event.target.parentElement.parentElement.lastElementChild.imageContent= "XL";
+  }
+
+  let colorOrange = document.querySelectorAll('.orange-option');
+  colorOrange[0].addEventListener('click', clickColorOrange);
+  colorOrange[1].addEventListener('click', clickColorOrange);
+  colorOrange[2].addEventListener('click', clickColorOrange);
+
+function clickColorGreen(event) {
+  event.target.parentElement.parentElement.lastElementChild.imageContent= "XL";
+  }
+
+  let colorGreen = document.querySelectorAll('.green-option');
+  colorGreen[0].addEventListener('click', clickColorGreen);
+  colorGreen[1].addEventListener('click', clickColorGreen);
+  colorGreen[2].addEventListener('click', clickColorGreen);
